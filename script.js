1b4b4d7d2f11fd36508d86132a38aab198f30cea
TINY PROGRAMMER <saqlainhassan1204@gmail.com>
2026-05-06 10:13:39 +0500

    Code upated

--- script.js
+++ script.js
@@ -1,44 +1,12 @@
-let tastlist=[];
-let elementlist=document.getElementById("tasklist");
+let balance=document.getElementById("balance");
+let Income=document.getElementById("Income");
+let Expence=document.getElementById("Expense");
 
-let name=document.getElementById("name");
+let History=document.getElementById("History");
+
+
+let text=document.getElementById("text");
 let amount=document.getElementById("amount");
 
 
-let income=document.getElementById("Income");
-let expence=document.getElementById("Expense");
 
-let btn =document.getElementById("btn");
-let list=document.getElementById("list");
-const balance=[];
-
-
-
-function GetData(){
-    let name=document.getElementById("name").value;
-    console.log(name);
-    let amount=document.getElementById("amount").value;
-console.log(amount);
-}
-if(name=salary || amount>=3000){
-    
-    DisplayPrice(amount);
-
-}
-
-
- function DisplayPrice(amount){
-balance.push(value);
-  RenderTask();
-
- }
-
-
-function RenderTask(){
-    balance.innerHTML="";
-    balance.foreach(item=>{
-        let l1=document.createElement("li");
-        l1.innerHTML=value;
-
-    });
-}
